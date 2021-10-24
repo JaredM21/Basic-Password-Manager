2598e9fec68d3bda2976642bd6c2d69d2fecdcd5
Jared Martinez <60527264+JaredM21@users.noreply.github.com>
2021-10-24 02:32:07 -0700

    Added basic encrpytion to passwords

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 #Import Packages
+from cryptography.fernet import Fernet
 
-master_pwd = input('Create your master password: ')
+''' #Used to get a key
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+#master_pwd = input('Create your master password: ')
+key = load_key() #+ master_pwd.encode()
+fer = Fernet(key)
 
 def view():
     with open('passwords.txt', 'r') as f:  # Automatically opens and closes file
         for line in f.readlines():
             data = (line.rstrip())
             user, passw = data.split('|')
-            print('User:', user, '', 'Password: ', passw)
+            print('User:', user, '', 'Password: ', fer.encrypt(passw.encode()).decode())
 
 
 def add():
     name = input('Account Name: ')
     pwd = input('Password: ')
 
-
     with open('passwords.txt', 'a') as f: #Automatically opens and closes file
-        f.write(name + '|' + pwd + '\n')
-
-
-
+        f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + '\n')
 
 while True:
     mode = input('Add password or view existing one (view, add)? Press q to quit ').lower()
